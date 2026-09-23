6c6e606f3af35d492d95106aa6b63b5e5e9565f6
Nazir Ahmed <fa23-bds-032@isbstudent.comsats.edu.pk>
2026-09-23 13:39:30 +0500

    Add Jenkins CI/CD pipeline success message

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins CI/CD pipeline!");
+
+        
         System.out.println("Build triggered successfully by GitHub push.");
 
         
