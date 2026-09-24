748ce2e5c197374db3b4cbaafe4e9c2da3c2f777
Nazir Ahmed <fa23-bds-032@isbstudent.comsats.edu.pk>
2026-09-24 15:42:15 +0500

    Remove duplicate build trigger message

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         
         System.out.println("Build triggered successfully by GitHub push.");
-        System.out.println("Build triggered successfully by GitHub push.");
+        
 
         
     }
